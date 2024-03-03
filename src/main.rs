657b476c32d4256d078080796c487b8d3f42606c
kaushik raj <algorithmunloack@gmail.com>
2024-03-03 10:20:29 +0530

    The updated version +

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+
+use std::fs::File;
+use std::io::Write;
+
+fn main() -> std::io::Result<()> {
+    let file_name = "large_file.txt";
+    let num_lines = 500_000;
+    let target_word = "Rust";
+    let other_words = ["Python", "Java", "C++", "JavaScript", "Go", "Swift", "Kotlin"];
+
+    let content: String = (0..num_lines)
+        .map(|i| {
+            let word = if i % 100 == 0 {
+                target_word
+            } else {
+                other_words[i % other_words.len()]
+            };
+            format!("{} is a programming language.\n", word)
+        })
+        .collect();
+
+    let mut file = File::create(file_name)?;
+    write!(file, "{}", content)?;
+
+    println!("Generated {} with {} lines", file_name, num_lines);
+
+    Ok(())
+}
